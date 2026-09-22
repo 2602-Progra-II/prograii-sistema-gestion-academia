908b393ed05fa82109c0c406d6d50c4a98019614
fransheskabenitez <franbenitezmu@gmail.com>
2026-09-21 22:58:29 -0600

    Ajuste de estructura de prueba de facturación

--- src/test/java/sv/gestionacademica/service/FacturaServiceTest.java
+++ src/test/java/sv/gestionacademica/service/FacturaServiceTest.java
@@ -1,11 +1,8 @@
-package com.academia;
+package sv.gestionacademica.service;
 import sv.gestionacademica.entity.Factura;
-import sv.gestionacademica.service.FacturaService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import sv.gestionacademica.entity.Factura;
-import sv.gestionacademica.service.FacturaService;
 
 import java.time.LocalDate;
 
